434912da996cfb64ba4014b16eec83838a129a8b
AmirHosseinHasanloo <AmirHosseinHasanloo2005@gmail.com>
2025-04-12 16:24:46 +0330

    Add project files.

---
+++ API_Shop/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace API_Shop.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        // navigation properties
+        public ICollection<Product> Products { get; set; }
+    }
+}
---
+++ API_Shop/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API_Shop.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+
+        // navigation properties
+        [ForeignKey(nameof(CategoryId))]
+        public Category Category { get; set; }
+    }
+}
